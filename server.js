92c473f46bb79f16b63665dbd9d866da3be9948d
Akshay Dixit <akshaydixi@gmail.com>
2013-07-02 02:49:28 +0530

    added socket support to server.js

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ var port = 8000;
 app.set('views', __dirname + '/tpl');
 app.set('view engine','jade');
 app.engine('jade',require('jade').__express);
-
+app.use(express.static( dirname + '/public'));
 
 app.get('/', function(req,res){
     res.render("page");
     });
 
-app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
 console.log("[+] Listening on port " + port);
+
+io.sockets.on('connection',function(socket){
+    socket.emit('message',{message: 'Welcome!' });
+    socket.on('send',function(data){
+        socket.emit('message',data);
+    });
+});
+
