c48f246a505611443ce681846eb7635e34be9088
Akshay Dixit <akshaydixi@gmail.com>
2013-07-02 03:00:35 +0530

    __dirname bugfix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = 8000;
 app.set('views', __dirname + '/tpl');
 app.set('view engine','jade');
 app.engine('jade',require('jade').__express);
-app.use(express.static( dirname + '/public'));
+app.use(express.static( __dirname + '/public'));
 
 app.get('/', function(req,res){
     res.render("page");
